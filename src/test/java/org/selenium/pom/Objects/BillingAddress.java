9f49070fcc12d94cf62116c6fc19d7dc59eabecc
lamdiepthao <thaolam.ldt98@gmail.com>
2025-12-18 12:28:26 +0700

    video 10 - 02

---
+++ src/test/java/org/selenium/pom/Objects/BillingAddress.java
@@ -0,0 +1,60 @@
+package org.selenium.pom.Objects;
+
+public class BillingAddress {
+    private String firstName;
+    private String lastName;
+    private String addressLineOne;
+    private String city;
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getAddressLineOne() {
+        return addressLineOne;
+    }
+
+    public void setAddressLineOne(String addressLineOne) {
+        this.addressLineOne = addressLineOne;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getPostCode() {
+        return postCode;
+    }
+
+    public void setPostCode(String postCode) {
+        this.postCode = postCode;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    private String postCode;
+    private String email;
+
+}
